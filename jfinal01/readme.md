6aaa314cce1e546d7fac2a496f29537ec909c379
max.chen <amingccc@gmail.com>
2019-09-12 23:50:32 +0800

    路由

---
+++ jfinal01/readme.md
@@ -0,0 +1,62 @@
+JFinal从入门到入魔教程
+
+# Jfinal3.0系列教程
+## 第一讲 环境和helloworld
+### 使用Eclipse+Maven构建JFinal的HelloWorld项目步骤：
+- 配置MAVEN
+- 安装eclipse Maven插件
+- 设置eclipse Maven插件的相关配置
+- pom.xml中填写JFinal的相关依赖描述，并且等待依赖包下载完成
+- 创建MyConfig配置类
+- web.xml中进行JFinal配置
+- 建立Controller，并且继承JFinal的Controller
+- 建立Route映射
+- 编写JFinal的Jetty启动方法，并且启动项目
+- 浏览器输入访问地址，看结果
+
+## 第二讲 JFinalConfig
+- 此方法用来配置`JFinal`常量 值，如开发模式常量 值，如开发模式`devMode`的配置 ，如下代码配置了运行在开发模式       
+    ```java
+    public void configConstant(Constants me) {
+        me.setDevMode(true);
+    }
+    ```
+- `PropKit`工具类用来操作外部配置文件。PropKit可以极度方便地在系统任意时空使用，如下     
+    ```java
+    public void configConstant(Constants me) {
+        // 第一次使用use加载的配置将成为主配置，可以通过PropKit.get(...)直接取值
+        PropKit.use("a_little_config.txt");
+        me.setDevMode(PropKit.getBoolean("devMode"));
+    }
+    ```
+
+## 第三讲 路由
+能区分下面两行的区别
+```java
+
+add("/blog", BlogController.class);
+add("/blog", BlogController.class,"/blog");
+```
+
+### 路由分包
+
+## 第十四讲.AOP简述
+### Interceptor的分类
+- Method Interceptor 方法级别
+- Class Interceptor 类级别 当前类中的所有方法都有效
+- Router Interceptor 路由级别 当前路由中所有方法都有效
+- Global Interceptor 全局拦截 web请求所有的请求方法进行拦截
+- Inject Interceptor 业务注入拦截器 对指定需要被注入的方法有效，可以是整个业务类，也可以某个方法
+
+拦截器栈 多个Interceptor进行组合形成的拦截器栈
+
+注解：
+Before 添加拦截
+Clear  清除拦截
+
+## 第20讲.Interceptor总结	
+### 拦截器的优先级
+1、Global
+2、Inject（InjectInterceptor RouteInterceptor）
+3、Class
+4、Method
--- jfinal01/src/main/java/com/demo/common/AppConfig.java
+++ jfinal01/src/main/java/com/demo/common/AppConfig.java
@@ -10,39 +10,46 @@ import com.jfinal.template.Engine;
  * @created 2019-09-12-21:13.
  */
 public class AppConfig extends JFinalConfig {
-	public void configConstant(Constants constants) {
+    public void configConstant(Constants constants) {
+        constants.setDevMode(true);
+    }
 
-	}
+    public void configRoute(Routes routes) {
+        routes.add("/", IndexController.class);
+        routes.add("/blog", IndexController.class);
+        routes.add("/abc", IndexController.class,"/blog");
+    }
 
-	public void configRoute(Routes routes) {
-		routes.add("/", IndexController.class);
-	}
+    public void configEngine(Engine engine) {
 
-	public void configEngine(Engine engine) {
+    }
 
-	}
+    public void configPlugin(Plugins plugins) {
 
-	public void configPlugin(Plugins plugins) {
+    }
 
-	}
+    public void configInterceptor(Interceptors interceptors) {
 
-	public void configInterceptor(Interceptors interceptors) {
+    }
 
-	}
+    public void configHandler(Handlers handlers) {
 
-	public void configHandler(Handlers handlers) {
+    }
 
-	}
+    public static void main(String[] args) {
+        /**
+         * 特别注意：Eclipse 之下建议的启动方式
+         */
+        // JFinal.start("src/main/webapp", 80, "/", 5);
 
-	public static void main(String[] args) {
-		/**
-		 * 特别注意：Eclipse 之下建议的启动方式
-		 */
-		// JFinal.start("src/main/webapp", 80, "/", 5);
+        /**
+         * 特别注意：IDEA 之下建议的启动方式，仅比 eclipse 之下少了最后一个参数
+         */
+        JFinal.start("src/main/webapp", 80, "/");
+    }
 
-		/**
-		 * 特别注意：IDEA 之下建议的启动方式，仅比 eclipse 之下少了最后一个参数
-		 */
-		JFinal.start("src/main/webapp", 80, "/");
-	}
+    @Override
+    public void afterJFinalStart() {
+        System.out.println("afterJFinalStart............可以用来启动任务，相当于回调");
+    }
 }
