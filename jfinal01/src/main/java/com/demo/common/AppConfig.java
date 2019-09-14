c32aee22819af139fa86c205f6f4cec0e69d3c24
max.chen <amingccc@gmail.com>
2019-09-14 21:00:21 +0800

    定时任务代码

--- jfinal01/src/main/java/com/demo/common/AppConfig.java
+++ jfinal01/src/main/java/com/demo/common/AppConfig.java
@@ -4,13 +4,17 @@ import com.demo.interceptor.GlobalInterceptor;
 import com.demo.model.Blog;
 import com.demo.route.AdminRoute;
 import com.demo.route.FrontRoute;
+import com.demo.service.ShareMethod;
+import com.demo.service.ShareObject;
 import com.jfinal.config.*;
 import com.jfinal.core.JFinal;
 import com.jfinal.ext.handler.ContextPathHandler;
 import com.jfinal.kit.PathKit;
 import com.jfinal.kit.PropKit;
 import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
+import com.jfinal.plugin.cron4j.Cron4jPlugin;
 import com.jfinal.plugin.druid.DruidPlugin;
+import com.jfinal.plugin.ehcache.EhCachePlugin;
 import com.jfinal.template.Engine;
 
 /**
@@ -18,70 +22,83 @@ import com.jfinal.template.Engine;
  * @created 2019-09-12-21:13.
  */
 public class AppConfig extends JFinalConfig {
-	public void configConstant(Constants constants) {
-		// 加载少量必要配置，随后可用PropKit.get(...)获取值
-		PropKit.use("a_little_config.txt");
-		constants.setDevMode(PropKit.getBoolean("devMode", false));
-	}
+    public void configConstant(Constants constants) {
+        // 加载少量必要配置，随后可用PropKit.get(...)获取值
+        PropKit.use("a_little_config.txt");
+        constants.setDevMode(PropKit.getBoolean("devMode", false));
+    }
 
-	public void configRoute(Routes routes) {
-		// routes.add("/", IndexController.class);
-		// routes.add("/", IndexController.class, "/abc");
-		// routes.add("/blog", BlogController.class, "/blog");
-		// routes.add("/user", UserController.class, "/user");
-		routes.add(new AdminRoute());// 后端路由
-		routes.add(new FrontRoute());// 前端路由
-	}
+    public void configRoute(Routes routes) {
+        // routes.add("/", IndexController.class);
+        // routes.add("/", IndexController.class, "/abc");
+        // routes.add("/blog", BlogController.class, "/blog");
+        // routes.add("/user", UserController.class, "/user");
+        routes.add(new AdminRoute());// 后端路由
+        routes.add(new FrontRoute());// 前端路由
+    }
 
-	public void configEngine(Engine engine) {
+    public void configEngine(Engine engine) {
+        engine.setBaseTemplatePath(PathKit.getWebRootPath());
+        engine.addSharedFunction("/front/common.html");
+        engine.addSharedStaticMethod(ShareObject.class);
+        engine.addSharedMethod(new ShareMethod());
+        engine.addSharedObject("sm",new ShareMethod());
 
-	}
+    }
 
-	public void configPlugin(Plugins plugins) {
-		// 配置 druid 数据库连接池插件
-		DruidPlugin druidPlugin = new DruidPlugin(PropKit.get("jdbcUrl"), PropKit.get("user"),
-				PropKit.get("password").trim());
-		plugins.add(druidPlugin);
+    public void configPlugin(Plugins plugins) {
+        // 配置 druid 数据库连接池插件
+        DruidPlugin druidPlugin = new DruidPlugin(PropKit.get("jdbcUrl"), PropKit.get("user"),
+                PropKit.get("password").trim());
+        plugins.add(druidPlugin);
 
-		// 配置ActiveRecord插件
-		ActiveRecordPlugin arp = new ActiveRecordPlugin(druidPlugin);
-		arp.setBaseSqlTemplatePath(PathKit.getWebRootPath() + "/WEB-INF");
-		arp.addSqlTemplate("sql/demo.sql");
-		arp.addMapping("blog", Blog.class);
-		plugins.add(arp);
-	}
+        // 配置ActiveRecord插件
+        ActiveRecordPlugin arp = new ActiveRecordPlugin(druidPlugin);
+        arp.setBaseSqlTemplatePath(PathKit.getWebRootPath() + "/WEB-INF");
+        arp.addSqlTemplate("sql/demo.sql");
+        arp.addMapping("blog", Blog.class);
+        plugins.add(arp);
 
-	public void configInterceptor(Interceptors interceptors) {
-		interceptors.add(new GlobalInterceptor());
-		// interceptors.addGlobalServiceInterceptor(new
-		// InjectInterceptor());//给所有的Service添加拦截器
-	}
+        //缓存插件
+        plugins.add(new EhCachePlugin());
 
-	public void configHandler(Handlers handlers) {
-		// 创建上下文
-		handlers.add(new ContextPathHandler("ctx"));
-	}
+        //任务调度插件
 
-	public static void main(String[] args) {
-		/**
-		 * 特别注意：Eclipse 之下建议的启动方式
-		 */
-		// JFinal.start("src/main/webapp", 80, "/", 5);
+        Cron4jPlugin cron4jPlugin = new Cron4jPlugin(PropKit.use("cron4j.properties"), "cron4j");
+        plugins.add(cron4jPlugin);
+    }
 
-		/**
-		 * 特别注意：IDEA 之下建议的启动方式，仅比 eclipse 之下少了最后一个参数
-		 */
-		JFinal.start("src/main/webapp", 80, "/");
-	}
+    public void configInterceptor(Interceptors interceptors) {
+        interceptors.add(new GlobalInterceptor());
+        // interceptors.addGlobalServiceInterceptor(new
+        // InjectInterceptor());//给所有的Service添加拦截器
+    }
 
-	@Override
-	public void afterJFinalStart() {
-		System.out.println("afterJFinalStart............可以用来启动任务，相当于回调");
-	}
+    public void configHandler(Handlers handlers) {
+        // 创建上下文
+        handlers.add(new ContextPathHandler("ctx"));
+    }
 
-	@Override
-	public void beforeJFinalStop() {
-		super.beforeJFinalStop();
-		System.out.println("beforeJFinalStop...");
-	}
+    public static void main(String[] args) {
+        /**
+         * 特别注意：Eclipse 之下建议的启动方式
+         */
+        // JFinal.start("src/main/webapp", 80, "/", 5);
+
+        /**
+         * 特别注意：IDEA 之下建议的启动方式，仅比 eclipse 之下少了最后一个参数
+         */
+        JFinal.start("src/main/webapp", 80, "/");
+    }
+
+    @Override
+    public void afterJFinalStart() {
+        System.out.println("afterJFinalStart............可以用来启动任务，相当于回调");
+    }
+
+    @Override
+    public void beforeJFinalStop() {
+        super.beforeJFinalStop();
+        System.out.println("beforeJFinalStop...");
+    }
 }
---
+++ jfinal01/src/main/java/com/demo/task/Task.java
@@ -0,0 +1,12 @@
+package com.demo.task;
+
+/**
+ * @author Chen
+ * @created 2019-09-14-14:30.
+ */
+public class Task implements Runnable{
+    public void run() {
+        System.out.println("Task Execute");
+    }
+
+}
